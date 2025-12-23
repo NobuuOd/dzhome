a2e7619009a069a9b3511f438d5d635aa08b7b77
Nobu <ioshiko1993@mail.ru>
2025-12-23 18:25:33 +0300

    domashka

---
+++ index.js
@@ -0,0 +1,24 @@
+// let array = [4, 6, 8, 17];
+// function arrayDouble(array) {
+//   for (let i = 0; i < array.length; i++) {
+//     array[i] = array[i] * 2;
+//   }
+//   console.log(array);
+// }
+// arrayDouble(array);
+
+// function sayHello(name1, name2) {
+//   console.log("Hello " + name1 +  " and " + name2 );
+// }
+// sayHello("Danya", "Nikita");
+
+// function selectDiff(num1, num2, detect) {
+//   if (detect == true) {
+//     let diff1 = num1 - num2;
+//     console.log(diff1);
+//   } else {
+//     let diff2 = num2 - num1;
+//     console.log(diff2);
+//   }
+// } 
+// selectDiff(5, 13, false)
