341db48487aaf0bfa937e34fce39d1a9a155c2ff
nikita <ioshiko1993@mail.ru>
2026-01-08 20:54:40 +0300

    dz2

--- index.js
+++ index.js
@@ -1,24 +1,14 @@
-// let array = [4, 6, 8, 17];
-// function arrayDouble(array) {
-//   for (let i = 0; i < array.length; i++) {
-//     array[i] = array[i] * 2;
-//   }
-//   console.log(array);
-// }
-// arrayDouble(array);
 
-// function sayHello(name1, name2) {
-//   console.log("Hello " + name1 +  " and " + name2 );
+// function print() {
+//     for (let i = 15; i >= 1; i--) {
+//         console.log(i);
+//     }
 // }
-// sayHello("Danya", "Nikita");
+// print()
 
-// function selectDiff(num1, num2, detect) {
-//   if (detect == true) {
-//     let diff1 = num1 - num2;
-//     console.log(diff1);
-//   } else {
-//     let diff2 = num2 - num1;
-//     console.log(diff2);
-//   }
-// } 
-// selectDiff(5, 13, false)
+// function print() {
+//     for (let i = 1; i <= 15; i++) {
+//         console.log(i);
+//     }
+// }
+// print()
