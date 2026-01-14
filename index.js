0950bf6276b1bba6d7e454a6b5755c6402f80a0a
nikita <ioshiko1993@mail.ru>
2026-01-14 14:32:37 +0300

    Старался

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
-let massive = [2, 3, 6];
-let proizvedenie = 1;
-function calce(massive) {
-    for (let i = 0; i < massive.length; i++) {
-        proizvedenie *= massive[i];
-    }
-    console.log(proizvedenie);
-}
-calce(massive);
+
+// let massive = [2, 8, 14, 18, 26];
+// function inhalf(massive) {
+//     for (let i = 0; i < massive.length; i++) {
+//         if (massive[i] > 10) {
+//             massive[i] = massive[i] / 2;
+//         }
+//     }
+//     console.log(massive);
+// }
+// inhalf(massive);
+
+// let massive = [3, 6, 13, 16, 10];
+// let sum = 0;
+// function arr(massive) {
+//     for (let i = 0; i < massive.length; i++) {
+//         if (massive[i] % 2 == 1) {
+//             sum += massive[i];
+//         }
+//     }
+//     console.log(sum);
+// }
+// arr(massive);
+
+
+// function say(name, hello) {
+//     if (hello) {
+//         console.log('Hello ' + name);
+//     } else {
+//         console.log('Goodbye ' + name);
+//     }
+// }
+// say('Nikita', true) // 'Hello Nikita'
+// say('Nikita', false) // 'Goodbye Nikita'
