d93cd93a0901309cc7b1a455d6925b32105e1f59
nikita <ioshiko1993@mail.ru>
2026-01-19 19:17:14 +0300

    устал

--- index.js
+++ index.js
@@ -1,34 +1,55 @@
 
-// let massive = [2, 8, 14, 18, 26];
-// function inhalf(massive) {
-//     for (let i = 0; i < massive.length; i++) {
-//         if (massive[i] > 10) {
-//             massive[i] = massive[i] / 2;
-//         }
-//     }
-//     console.log(massive);
-// }
-// inhalf(massive);
-
-// let massive = [3, 6, 13, 16, 10];
-// let sum = 0;
-// function arr(massive) {
-//     for (let i = 0; i < massive.length; i++) {
-//         if (massive[i] % 2 == 1) {
-//             sum += massive[i];
-//         }
-//     }
-//     console.log(sum);
-// }
-// arr(massive);
-
-
-// function say(name, hello) {
-//     if (hello) {
-//         console.log('Hello ' + name);
-//     } else {
-//         console.log('Goodbye ' + name);
+// function write() {
+//     for (let i = 0; i <= 20; i++) {
+//       if (i % 2 == 1) {
+//         console.log(i);
+//       }
 //     }
 // }
-// say('Nikita', true) // 'Hello Nikita'
-// say('Nikita', false) // 'Goodbye Nikita'
+// write();
+
+// let massive = [-1, 2, -3, 4];
+// function countPositive(massive) {
+//   let plus = 0;
+//   for (i = 0; i < massive.length; i++) {
+//     if (massive[i] > 0) {
+//       plus++;
+//     }
+//   }
+//   console.log(plus);
+// }
+// countPositive([-1, 2, -3, 4]) // 2
+// countPositive([1, 2, -3, 4]) // 3
+
+// function reverse(massive) {
+//   for (i = 0; i < massive.length; i++) {
+//     if (massive[i] < 0) {
+//       massive[i] = massive[i] * -1;
+//     }
+//   }
+//   console.log(massive);
+// }
+// reverse([-1, 2, -3, 4]) // [1, 2, 3, 4]
+// reverse([-10, 10, 4, -11, -6, 0]) // [10, 10, 4, 11, 6, 
+
+  // function select(array1, array2) {
+  //    if (array1.length > array2.length) {
+  //     console.log(array1);
+  //    } else {
+  //     console.log(array2);
+  //    }
+  // }
+  // select([1, 2, 3], [-1, -2, -3, -4]) // [-1, -2, -3, -4]
+  // select([1, 2, 3, 4, 5], [-1, -2, -3, -4]) // [1, 2, 3, 4, 5]
+
+  // function massGreet(name) {
+  //   let result = 'Hello ' + name[0];
+  //   for(let i = 1; i < name.length - 1; i++) {
+  //     result = result + ', ' + name[i];
+  //   }
+  //    result = result + ' and ' + name[name.length -1];
+  //   console.log(result);
+  // }
+  // massGreet(['Nikita', 'Danila', 'Kirill']) // Hello Nikita, Danila and Kirill
+  // massGreet(['Sonya', 'Katya', 'Nastya']) // Hello Sonya, Katya and Nastya
+
