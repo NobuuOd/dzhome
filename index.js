3ad10cba2adbf5a14505c75885453a8a10da06b6
Nobu <ioshiko1993@mail.ru>
2026-03-04 19:20:25 +0300

    stroki

--- index.js
+++ index.js
@@ -1,52 +1,39 @@
-function isEven(number) {
-  if (number % 2 == 0) {
+function isShort(str) {
+  if (str.length < 4) {
     return true;
   } else {
     return false;
   }
 }
-// console.log(isEven(4));
-// console.log(isEven(3));
+// console.log(isShort('jhgllo'));
 
-function evenArray(massive) {
+function shortArray(massive) {
   let newMassive = [];
   for (let i = 0; i < massive.length; i++) {
-    if (isEven(massive[i])) {
+    if (isShort(massive[i])) {
       newMassive.push(massive[i]);
     }
   }
   return newMassive;
 }
-// console.log(evenArray([14, 3, 6, 7]));
-// console.log(evenArray([12, 6, 17, 23]));
+// console.log(shortArray(["окно", "кнопка", "программа", "лук"]));
+// console.log(shortArray(["три", "кнопка", "мир", "лук"]));
 
-function oddArray(massive) {
-  let countChet = 0;
-  let countNechet = 0;
+function wordsArray(massive) {
+  let longWords = 0;
+  let shortWords = 0;
   for (let i = 0; i < massive.length; i++) {
-    if (isEven(massive[i])) {
-      countChet = countChet + 1;
+    if (isShort(massive[i])) {
+      shortWords = shortWords + 1;
     } else {
-      countNechet = countNechet + 1;
+      longWords = longWords + 1;
     }
   }
-  if (countChet > countNechet) {
+  if (shortWords > longWords) {
     return true;
   } else {
     return false;
   }
 }
-// console.log(oddArray([13, 12, 6, 8]));
-// // console.log(oddArray([3, 12, 5, 8]));
-
-function numberArray(massive) {
-  let countNum = 0;
-  for (let i = 0; i < massive.length; i++) {
-    if (massive[i] % 2 == 0) {
-      countNum = countNum + 1;
-    }
-  }
-  return countNum;
-}
-// console.log(numberArray([4, 6, 10, 16]));
-// console.log(numberArray([4, 6, 10, 16, 18, 20]));
+// console.log(wordsArray(['ключ', 'помидор', 'кот', 'снег']));
+// console.log(wordsArray(['луга', 'вор', 'сок', 'мор']));
