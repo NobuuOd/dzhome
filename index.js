5ca48f2108a38293a806c86bea39f409724acdf4
Nobu <ioshiko1993@mail.ru>
2026-03-25 19:43:47 +0300

    nst4

--- index.js
+++ index.js
@@ -1,45 +1,98 @@
-// function isShort(str) {
-//   if (str.length < 4) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-// console.log(isShort('jhgh'));
+function prosChisla(a) {
+  let bypass = 1;
+  for (let i = 2; i < a; i++) {
+    if (a % i == 0) {
+      return false;
+    }
+  }
+  if (a == bypass) {
+    return false;
+  }
+  return true;
+}
+// console.log(prosChisla(1));
 
-function shortArray(massive) {
+function increasing(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+// console.log(increasing([1, 2, -3, -4]));
+
+function everyFive(array) {
   let newMassive = [];
-  for (let i = 0; i < massive.length; i++) {
-    if (isShort(massive[i])) {
-      newMassive.push(massive[i]);
+  for (let i = 0; i < array.length; i++) {
+    if ((i + 1) % 5 != 0) {
+      newMassive.push(array[i]);
     }
   }
   return newMassive;
 }
-// console.log(shortArray(["окно", "кнопка", "программа", "лук"]));
-// console.log(shortArray(["три", "кнопка", "мир", "лук"]));
+// console.log(everyFive([1, 2, 3, 4, 5, 6]));
+// console.log(everyFive([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
+// console.log(everyFive(["dsa", "ads1", "dud", "asdw", "frfrf", "tyuik"]));
 
-function wordsArray(massive) {
-  let longWords = 0;
-  let shortWords = 0;
-  for (let i = 0; i < massive.length; i++) {
-    if (isShort(massive[i])) {
-      shortWords = shortWords + 1;
-    } else {
-      longWords = longWords + 1;
+function summaArray(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+  }
+  return sum;
+}
+
+function arrayMatrix(array) {
+  let sum = 0;
+  for (let j = 0; j < array.length; j++) {
+    sum += summaArray(array[j]);
+  }
+  return sum;
+}
+// console.log(
+//   arrayMatrix([
+//     [1, 2, 3],
+//     [4, 5, 6],
+//     [7, 8, 9],
+//   ]),
+// );
+
+function isAdult(age) {
+  if (age >= 18) {
+    return true;
+  }
+  return false;
+}
+// console.log(isAdult(18));
+
+function ageArray(array) {
+  let newAgeArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (isAdult(array[i])) {
+      newAgeArray.push(array[i]);
     }
   }
- return shortWords > longWords;
+  return newAgeArray;
 }
-// console.log(wordsArray(['ключ', 'помидор', 'кот', 'снег']));
-// console.log(wordsArray(['луга', 'вор', 'сок', 'мор']));
+// console.log(ageArray([19, 15, 6, 18, 24]));
+// console.log(ageArray([7, 33, 8, 17, 9]));
 
-
-
-function isShort(str) {
- return str.length < 4;
+function rangeAge(array) {
+  let adultArray = 0;
+  let minorArray = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (isAdult(array[i])) {
+      adultArray = adultArray + 1;
+    } else {
+      minorArray = minorArray + 1;
+    }
+  }
+  if (adultArray > minorArray) {
+    return true;
+  } else {
+    return false;
+  }
 }
-// console.log(isShort('jhgd'));
-
-array = [[1, 2, 45, 16, 17], ['луга', 'вор', 'сок', 'мор']]
-// console.log(array[0][1]);
+// console.log(rangeAge([18, 14, 20, 6]));
+// console.log(rangeAge([18, 19, 20, 6]));
