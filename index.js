e1e43a5ea6cf21333561b3959aefdbc5c0f263f5
Nobu <ioshiko1993@mail.ru>
2026-04-13 19:11:06 +0300

    tyrg

--- index.js
+++ index.js
@@ -1,98 +1,193 @@
-function prosChisla(a) {
-  let bypass = 1;
-  for (let i = 2; i < a; i++) {
-    if (a % i == 0) {
-      return false;
+let fruits = [
+  { name: "яблоко", color: "красное", price: 150 },
+  { name: "банан", color: "желтый", price: 120 },
+  { name: "апельсин", color: "оранжевый", price: 180 },
+];
+function maxPrice(array) {
+  let major = 0;
+  let majorName = "";
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].price > major) {
+      major = array[i].price;
+      majorName = array[i].name;
     }
   }
-  if (a == bypass) {
+  return majorName;
+}
+// console.log(maxPrice(fruits));
+
+// let thingsShop = [
+//   { object: "обувь", name: "лоферы", size: 44 },
+//   { object: "брюки", name: "чинос", size: 54 },
+//   { object: "рубашка", name: "поло", size: 56 },
+// ];
+function objectChecks(obj) {
+  if (obj.name == undefined) {
     return false;
-  }
-  return true;
-}
-// console.log(prosChisla(1));
-
-function increasing(array) {
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] > array[i + 1]) {
-      return false;
-    }
-  }
-  return true;
-}
-// console.log(increasing([1, 2, -3, -4]));
-
-function everyFive(array) {
-  let newMassive = [];
-  for (let i = 0; i < array.length; i++) {
-    if ((i + 1) % 5 != 0) {
-      newMassive.push(array[i]);
-    }
-  }
-  return newMassive;
-}
-// console.log(everyFive([1, 2, 3, 4, 5, 6]));
-// console.log(everyFive([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]))
-// console.log(everyFive(["dsa", "ads1", "dud", "asdw", "frfrf", "tyuik"]));
-
-function summaArray(array) {
-  let sum = 0;
-  for (let i = 0; i < array.length; i++) {
-    sum += array[i];
-  }
-  return sum;
-}
-
-function arrayMatrix(array) {
-  let sum = 0;
-  for (let j = 0; j < array.length; j++) {
-    sum += summaArray(array[j]);
-  }
-  return sum;
-}
-// console.log(
-//   arrayMatrix([
-//     [1, 2, 3],
-//     [4, 5, 6],
-//     [7, 8, 9],
-//   ]),
-// );
-
-function isAdult(age) {
-  if (age >= 18) {
-    return true;
-  }
-  return false;
-}
-// console.log(isAdult(18));
-
-function ageArray(array) {
-  let newAgeArray = [];
-  for (let i = 0; i < array.length; i++) {
-    if (isAdult(array[i])) {
-      newAgeArray.push(array[i]);
-    }
-  }
-  return newAgeArray;
-}
-// console.log(ageArray([19, 15, 6, 18, 24]));
-// console.log(ageArray([7, 33, 8, 17, 9]));
-
-function rangeAge(array) {
-  let adultArray = 0;
-  let minorArray = 0;
-  for (let i = 0; i < array.length; i++) {
-    if (isAdult(array[i])) {
-      adultArray = adultArray + 1;
-    } else {
-      minorArray = minorArray + 1;
-    }
-  }
-  if (adultArray > minorArray) {
-    return true;
   } else {
-    return false;
+    return true;
   }
 }
-// console.log(rangeAge([18, 14, 20, 6]));
-// console.log(rangeAge([18, 19, 20, 6]));
+// console.log(objectChecks({name: "doora"}));
+
+function ffun(obj, key) {
+  if (obj[key] == undefined) {
+    return false;
+  } else {
+    return true;
+  }
+}
+// console.log(ffun({home: 0},'gol'));
+
+function generalPrice(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i].price;
+  }
+  return sum;
+}
+// console.log(generalPrice(fruits));
+
+const users = [
+  {
+    id: 1,
+    name: "Анна",
+    age: 25,
+    city: "Москва",
+    isActive: true,
+    salary: 80000,
+  },
+  {
+    id: 2,
+    name: "Борис",
+    age: 32,
+    city: "Санкт-Петербург",
+    isActive: false,
+    salary: 95000,
+  },
+  {
+    id: 3,
+    name: "Виктория",
+    age: 28,
+    city: "Москва",
+    isActive: true,
+    salary: 110000,
+  },
+  {
+    id: 4,
+    name: "Глеб",
+    age: 35,
+    city: "Казань",
+    isActive: true,
+    salary: 120000,
+  },
+  {
+    id: 5,
+    name: "Дарья",
+    age: 22,
+    city: "Москва",
+    isActive: false,
+    salary: 65000,
+  },
+  {
+    id: 6,
+    name: "Евгений",
+    age: 29,
+    city: "Санкт-Петербург",
+    isActive: true,
+    salary: 105000,
+  },
+  {
+    id: 7,
+    name: "Жанна",
+    age: 31,
+    city: "Казань",
+    isActive: true,
+    salary: 115000,
+  },
+  {
+    id: 8,
+    name: "Захар",
+    age: 26,
+    city: "Москва",
+    isActive: false,
+    salary: 70000,
+  },
+];
+function usName(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log("Пользователь: " + [array[i].name]);
+  }
+}
+// usName(users);
+
+function userList(array) {
+  let activeUsers = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].isActive == true) {
+      activeUsers.push(array[i]);
+    }
+  }
+  return activeUsers;
+}
+// console.log(userList(users));
+
+function userAge(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].name == "Глеб") {
+      return array[i].age;
+    }
+  }
+}
+// console.log(userAge(users));
+
+function loginUs(array) {
+  let userNames = [];
+  for (let i = 0; i < array.length; i++) {
+    userNames.push(array[i].name);
+  }
+  return userNames;
+}
+// console.log(loginUs(users));
+
+function moneySalary(array) {
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i].salary;
+  }
+  return sum;
+}
+// console.log(moneySalary(users));
+
+const books = [
+  { title: "1984", author: "Джордж Оруэлл", year: 1949, genre: "антиутопия" },
+  {
+    title: "Скотный двор",
+    author: "Джордж Оруэлл",
+    year: 1945,
+    genre: "сатира",
+  },
+  {
+    title: "Мастер и Маргарита",
+    author: "Михаил Булгаков",
+    year: 1967,
+    genre: "роман",
+  },
+  {
+    title: "Собачье сердце",
+    author: "Михаил Булгаков",
+    year: 1925,
+    genre: "повесть",
+  },
+];
+function nameBooks(array, author, year) {
+  let authorName = [];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i].author == author && array[i].year > year) {
+      authorName.push(array[i]);
+    }
+  }
+  return authorName;
+}
+// console.log(nameBooks(books, "Михаил Булгаков", 1947));
+
